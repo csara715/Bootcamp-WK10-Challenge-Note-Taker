6885a9b283c9acfb3e88ffd4897fc61686b8b587
csara <csara715@gmail.com>
2023-01-29 16:23:37 -0700

    Mostly done but delete route works in Insomnia but not on site

---
+++ routes/notes.js
@@ -0,0 +1,43 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../public/assets/js/fsUtils");
+
+// GET Route for retrieving all notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json("Note was successfully created");
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+      writeToFile("./db/db.json", result);
+      res.json(`Item ${noteId} has been deleted`);
+    });
+});
+
+module.exports = notes;
